7dd759ac5d7a0c01483125c55cd0e3eb65bcad3d
sunlishan <sunlishan@xiaomi.com>
2016-02-02 09:28:05 +0800

    Calendor

---
+++ Calendor/app/src/main/java/pri/samarium/calendar/MainActivity.java
@@ -0,0 +1,66 @@
+package pri.samarium.calendar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "sls.activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.i(TAG, "onCreate");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG, "onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i(TAG, "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy");
+        super.onDestroy();
+    }
+}
+
---
+++ Calendor/app/src/main/java/pri/samarium/calendar/ui/MyCalendor.java
@@ -0,0 +1,40 @@
+package pri.samarium.calendar.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by sunlishan on 16-2-1.
+ */
+public class MyCalendor extends View {
+    public MyCalendor(Context context) {
+        super(context);
+    }
+
+    public MyCalendor(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyCalendor(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+
+}
